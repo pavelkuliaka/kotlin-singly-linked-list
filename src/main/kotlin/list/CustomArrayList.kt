83adef80a71165ca07b43c38de4d29a4ef133a02
Pavel Kuliaka <pavel.kulyaka@yandex.ru>
2026-05-13 20:41:15 +0300

    Implemented CustomArrayList

--- src/main/kotlin/list/CustomArrayList.kt
+++ src/main/kotlin/list/CustomArrayList.kt
@@ -1,18 +1,75 @@
 package org.example.list
 
-class CustomArrayList(startSize: Int): CustomList {
-    var inner = IntArray(startSize)
+class CustomArrayList(startSize: Int) : CustomList {
+    private var inner = IntArray(startSize)
+    private var _size = 0
 
-    // TODO: implement interface CustomList
+    override val size: Int
+        get() = _size
+
+    override fun get(index: Int): Int {
+        if (index !in 0..<_size) throw IndexOutOfBoundsException()
+        return inner[index]
+    }
+
+    override fun set(index: Int, value: Int) {
+        if (index !in 0..<_size) throw IndexOutOfBoundsException()
+        inner[index] = value
+    }
 
     override fun add(element: Int) {
-        TODO("implement this")
+        if (_size == inner.size) resize(inner.size * 2)
+        inner[_size++] = element
+    }
+
+    override fun addFirst(element: Int) {
+        if (_size == inner.size) resize(inner.size * 2)
+        for (i in _size downTo 1) {
+            inner[i] = inner[i - 1]
+        }
+        inner[0] = element
+        _size++
+    }
+
+    override fun indexOf(element: Int): Int {
+        for (i in 0..<_size) {
+            if (inner[i] == element) return i
+        }
+        return -1
+    }
+
+    override fun remove(element: Int): Boolean {
+        for (i in 0..<_size) {
+            if (inner[i] == element) {
+                for (j in i..<_size - 1) {
+                    inner[j] = inner[j + 1]
+                }
+                _size--
+                return true
+            }
+        }
+        return false
     }
 
     private fun resize(newSize: Int) {
-        TODO("implement this")
-        // should return new array list with newSize size
-        // and first size elements copied from this
+        val newInner = IntArray(newSize)
+        for (i in 0..<_size) {
+            newInner[i] = inner[i]
+        }
+        inner = newInner
+    }
+
+    override fun iterator(): Iterator<Int> {
+        return object : Iterator<Int> {
+            private var index = 0
+
+            override fun hasNext(): Boolean = index < _size
+
+            override fun next(): Int {
+                if (!hasNext()) throw NoSuchElementException()
+                return inner[index++]
+            }
+        }
     }
 
     companion object {

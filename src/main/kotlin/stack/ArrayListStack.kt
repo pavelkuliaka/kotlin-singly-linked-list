d45aaf22efd8c9c8dba0d3afab404a37d8ebcb61
Pavel Kuliaka <pavel.kulyaka@yandex.ru>
2026-05-13 21:02:45 +0300

    Implemented ArrayListStack

--- src/main/kotlin/stack/ArrayListStack.kt
+++ src/main/kotlin/stack/ArrayListStack.kt
@@ -1,5 +1,26 @@
 package org.example.stack
 
-class ArrayListStack : Stack {
-    // TODO: use delegation instead of inheritance here, use CustomArrayList
+import org.example.list.CustomArrayList
+import org.example.list.CustomList
+
+class ArrayListStack(capacity: Int) : CustomList by CustomArrayList(capacity), Stack {
+
+    override fun push(value: Int) {
+        addFirst(value)
+    }
+
+    override fun pop(): Int {
+        if (isEmpty) throw NoSuchElementException()
+        val value = get(0)
+        remove(value)
+        return value
+    }
+
+    override fun peek(): Int {
+        if (isEmpty) throw NoSuchElementException()
+        return get(0)
+    }
+
+    override val isEmpty: Boolean
+        get() = size == 0
 }
